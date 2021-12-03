df0401972c2ee5523149635e0ae80c65478cd131
Gabor Sebestyen <v-gsebestyen@expediagroup.com>
2021-12-03 12:06:30 +0100

    Day 1 solution
    Run with `cat ./Resources/day1_input.txt | swift run day1` command

--- Sources/day1/main.swift
+++ Sources/day1/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import Foundation
+
+guard let firstLine = readLine() else {
+  print("0")
+  exit(0)
+}
+
+
+var counter = UInt(0)
+
+var prevLine: String? = firstLine
+while let line = readLine() {
+  if let l = prevLine,
+    let d0 = UInt(l),
+    let d1 = UInt(line),
+    d0 < d1 {
+    counter += 1
+  }  
+
+  prevLine = line
+}
+
+print(counter)
